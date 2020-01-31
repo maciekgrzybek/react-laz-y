0a4d49a8f95a66a0be4e38429bb7335cb56628ac
Maciej Grzybek <maciekgrzybek1@gmail.com>
2020-01-31 11:17:14 +0000

    Setup jest and add shell tests

---
+++ __tests__/index.spec.tsx
@@ -0,0 +1,19 @@
+describe('children element', () => {
+  test('shows the children element, when state is set to visible', () => {});
+  test('pass the props to the children component', () => {});
+});
+
+describe('intersection observer', () => {
+  test('observer is being called with config passed from props', () => {});
+  test('callback function is being called, when state is set to visible', () => {});
+});
+
+describe('fallback', () => {
+  test('renders default fallback element, when no fallback is passed by props', () => {});
+  test('renders fallback passed in props', () => {});
+});
+
+describe('error handling', () => {
+  test('returns correct error message when children is not passed', () => {});
+  test('returns correct error message when more than one child is passed', () => {});
+});
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  setupFilesAfterEnv: [
+    '@testing-library/react/cleanup-after-each',
+    '@testing-library/jest-dom/extend-expect'
+  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};

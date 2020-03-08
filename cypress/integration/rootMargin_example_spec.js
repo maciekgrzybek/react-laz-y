573349e11f4027e4af5189eabdffbe8cbcaaf151
maciekgrzybek <maciekgrzybek1@gmail.com>
2020-03-08 17:01:54 +0000

    Add rootMargin test

--- cypress/integration/rootMargin_example_spec.js
+++ cypress/integration/rootMargin_example_spec.js
@@ -4,15 +4,15 @@ describe("Basic example", () => {
   });
 
   it("sucessfully loads", () => {
-    cy.visit("/");
+    cy.visit("/root-margin");
   });
   it("does not load lazy component when its wrapper is outside of the viewport", () => {
-    cy.scrollTo(0, 500);
+    cy.scrollTo(0, 100);
     cy.wait(1000);
     cy.get("#lazy-inner-wrapper").should("not.exist");
   });
   it("loads the lazy component when its wrapper scrolls into viewport", () => {
-    cy.scrollTo(0, 501);
+    cy.scrollTo(0, 101);
     cy.get("#lazy-inner-wrapper").should("to.exist");
   });
 });
